9fd8fbf26b1a97324f396478d72059f57e26f242
Lonswaya <alonswa@purdue.edu>
2015-01-25 00:09:09 -0500

    Added two scripts and gamedoc
    One script is for player movement, other is for a camera that follows
    the player.
    
    Gamedoc is not complete but contains an outline of what to expect

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	private bool up;
+	private bool down;
+	private bool right;
+	private bool left;
+	// Update is called once per frame
+	void Update() {
+		up = Input.GetKey (KeyCode.W);
+		down = Input.GetKey (KeyCode.S);
+		right = Input.GetKey (KeyCode.D);
+		left = Input.GetKey (KeyCode.A);
+
+
+		}
+	void FixedUpdate () {
+
+		if (right)
+		{
+			this.rigidbody2D.AddForce(new Vector2(80 * Time.deltaTime, 0));
+
+		}
+		if (left)
+		{
+			this.rigidbody2D.AddForce(new Vector2(-80 * Time.deltaTime, 0));
+		}
+		if (up)
+		{
+			this.rigidbody2D.AddForce(new Vector2(0,80 * Time.deltaTime));
+		}
+		if (down)
+		{
+			this.rigidbody2D.AddForce(new Vector2(0,-80 * Time.deltaTime));
+		}
+
+	}
+}
---
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+
+			point.Set (point.x - 1000, point.y, point.z);
+			point.y += 300;
+			//Vector3 point = new Vector3(camera.WorldToViewportPoint(target.position).x ,camera.WorldToViewportPoint(target.position).y) ;
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(.05f, .05f, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+		}
+	}
+}
