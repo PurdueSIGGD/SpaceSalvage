6908381ad90bb6719a89af218e658c21e10da8f6
Lonswaya <alonswa@purdue.edu>
2015-01-27 18:39:36 -0500

    Cleaned up and changed line color
    TODO: Create button to sling you back (like tape measure)

--- Assets/Scripts/TubeController.cs
+++ Assets/Scripts/TubeController.cs
@@ -9,7 +9,7 @@ public class TubeController : MonoBehaviour {
 	private bool ejectkey;
 	private bool ejected = false;
 	private int tubecount = 0; //because I am stupid and +- 1 errors are the bane of my existence
-	private float tubelength = 2;
+	private float tubelength = .5f;
 	private Vector3[] tubes; //There is no function to get a value at the index for LineRenderer
 	private float dist = 0;
 	private float timesincelast = 4;
@@ -37,33 +37,34 @@ public class TubeController : MonoBehaviour {
 						tubes [tubecount - 1] = this.transform.position; //Update position
 						LineRenderer l = (LineRenderer)GetComponent<LineRenderer> ();
 						if (tubecount > 2) { //In order to pick up tubes
-								if (timesincedrop > 2 && (Math.Abs (this.transform.position.x - ((Vector3)tubes [tubecount - 2]).x) < .8) && (Math.Abs (this.transform.position.y - ((Vector3)tubes [tubecount - 2]).y) < .8)) {
-										
-										tubecount--;
-										dist = 0;
-										tubesleft++;
-										l.SetVertexCount (tubecount);
-										timesincelast = 0;
-										tubes [tubecount] = new Vector3 (0, 0, 0); //clear value
-										
-								}
+							if (timesincedrop > 3 && (Math.Abs (this.transform.position.x - ((Vector3)tubes [tubecount - 2]).x) < .8) && (Math.Abs (this.transform.position.y - ((Vector3)tubes [tubecount - 2]).y) < .8) && Vector3.Magnitude (transform.position - tubes[tubecount - 1]) < 2) {
+											//print ("Picked up");
+											tubecount--;
+											dist = 0;
+											tubesleft++;
+											l.SetVertexCount (tubecount);
+											timesincelast = 0;
+											tubes [tubecount] = new Vector3 (0, 0, 0); //clear value
+											
+							}
 						}
+
 						dist = Vector3.Magnitude ((tubes [tubecount - 2] - transform.position)); //far away enough?
 						l.SetPosition (tubecount - 1, this.transform.position);
 						timesincelast += Time.deltaTime;
 						timesincedrop += Time.deltaTime;
-			if (dist > tubelength && tubesleft > 0 && (Vector3.Magnitude (transform.rigidbody2D.velocity) > 3 || timesincelast > 4 || dist > tubelength * 1.5 ) ) { //can we drop it like it is hot?
-								print (dist);
+
+						if (dist > tubelength  && tubesleft > 0 && (  timesincelast > 4  || dist > tubelength * 1.3 )) { //can we drop it like it is hot?
+
 								timesincedrop = 0;
 								tubesleft--;
 								tubecount++; 
-								tubes [tubecount - 1] = this.transform.position;
 								l.SetVertexCount (tubecount);
 								l.SetPosition (tubecount - 1, this.transform.position);
 								dist = 0;
 						}
 						
-						if ( tubesleft == 0) {
+						if ( tubesleft == 0 || dist > tubelength) {
 							this.transform.rigidbody2D.AddForce((tubes[tubecount - 2] - transform.position));
 				
 						}
