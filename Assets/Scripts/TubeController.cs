bf8f612a7c519bba124acde85c5223bdd65de16d
Lonswaya <alonswa@purdue.edu>
2015-01-28 16:41:19 -0500

    I tried to implement pulling a rope...
    It didn't work out so well. It is currently commented out, but line
    renderer is not meant to calculate something like that. In order to
    render that to full potential, it has to loop through each individual
    rope segment, and poke around with it. Got incredibly laggy once I got
    to around 25 segments. I say we don't implement it and find a
    workaround.
    
    Also cleaned up spare code.

--- Assets/Scripts/TubeController.cs
+++ Assets/Scripts/TubeController.cs
@@ -13,11 +13,10 @@ public class TubeController : MonoBehaviour {
 	private float tubelength = .5f;
 	private Vector3[] tubes; //There is no function to get a value at the index for LineRenderer
 	private float dist = 0;
-	private float timesincelast = 4;
-	private float timesincedrop = 4	;
 	// Use this for initialization
 	void Start () {
 		tubes = new Vector3[tubesleft + 1];
+
 		LineRenderer l = (LineRenderer) GetComponent<LineRenderer>();
 		l.SetColors(Color.cyan, Color.cyan);
 		l.SetVertexCount (2);
@@ -28,41 +27,55 @@ public class TubeController : MonoBehaviour {
 		tubesleft -= tubecount;
 		ejectkey = false;
 		returnkey = false;
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			
+		LineRenderer l = (LineRenderer)GetComponent<LineRenderer> ();
 		returnkey = Input.GetKey (KeyCode.Q);
 		if (!ejectkey && !returnkey) { //Stop everything if ejected
 					ejectkey = Input.GetKey (KeyCode.G);
-					tubes [tubecount - 1] = this.transform.position; //Update position
-					LineRenderer l = (LineRenderer)GetComponent<LineRenderer> ();
 					dist = Vector3.Magnitude ((tubes [tubecount - 2] - transform.position)); //far away enough?
-					l.SetPosition (tubecount - 1, this.transform.position);
-					timesincelast += Time.deltaTime;
-					timesincedrop += Time.deltaTime;
-
+					tubes [tubecount - 1] = this.transform.position; //Update position
+					
 					if (dist > tubelength  && tubesleft > 0) { //can we drop it like it is hot?
-
-							timesincedrop = 0;
 							tubesleft--;
 							tubecount++; 
 							l.SetVertexCount (tubecount);
-							l.SetPosition (tubecount - 1, this.transform.position);
+							l.SetPosition(tubecount - 1, this.transform.position); //If I don't call it (even when it is called right after that) it flashes for a second. Bothers me.
+
 							dist = 0;
 					}
-						
 					if ( tubesleft == 0 || dist > tubelength) {
 						this.transform.rigidbody2D.AddForce((tubes[tubecount - 2] - transform.position));
-				
 					}
 						
 		}
 
+
 	}
 	void FixedUpdate() {
+		LineRenderer l = (LineRenderer)GetComponent<LineRenderer> ();
+
+
+		/*for (int i = 1; i < tubecount; i++) { //line bending
+			if (returnkey && !ejectkey) {
+				if (Vector3.Magnitude(tubes[i] - tubes[i + 1]) < 2.5 && Vector3.Magnitude(tubes[i] - tubes[i - 1]) < 2.5 && !(tubecount == 2)) {
+					print ("moooooooove");
+					tubes[i] += (tubes[i + 1] - tubes[i]) / 20;
+				}
+
+			} else {
+				if (Vector3.Magnitude(tubes[i] - tubes[i + 1]) > 2 && Vector3.Magnitude(tubes[i] - tubes[i - 1]) > 2 && !(tubecount == 2)) {
+					print ("returnnnnnnn");
+					tubes[i] += (tubes[i - 1] - tubes[i]) / 20;
+				}
+			}
+
+			//tubes[i] = tubes[i] + tubevelocity[i];
+			l.SetPosition(i, tubes[i]);
+		}*/
+
 		if (ejected) {
 			ejectcooldown += Time.fixedDeltaTime;
 			if (ejectcooldown >= 4 && (Math.Abs(this.transform.position.x - ((Vector3)tubes [tubecount - 1]).x) < .6) && (Math.Abs (this.transform.position.y - ((Vector3)tubes [tubecount - 1]).y) < .6 )) {
@@ -72,7 +85,7 @@ public class TubeController : MonoBehaviour {
 			}
 		} 
 		else {
-			LineRenderer l = (LineRenderer)GetComponent<LineRenderer> ();
+
 			tubes [tubecount - 1] = this.transform.position; //Update position
 			l.SetPosition (tubecount - 1, this.transform.position);
 			if (returnkey) { //In order to pick up tubes
@@ -81,12 +94,12 @@ public class TubeController : MonoBehaviour {
 					dist = 0;
 					tubesleft++;
 					l.SetVertexCount (tubecount);
-					timesincelast = 0;
 					tubes [tubecount] = new Vector3 (0, 0, 0); //clear value
 
 
 				}
-				this.transform.rigidbody2D.AddForce(5 * (tubes[tubecount - 2] - transform.position));
+
+				//this.transform.rigidbody2D.AddForce(3 * (tubes[tubecount - 2] - transform.position));
 				returnkey = true;
 					
 				
