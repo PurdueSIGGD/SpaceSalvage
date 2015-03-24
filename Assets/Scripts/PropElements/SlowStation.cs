9196dc5c57aebe204bdcdf4cdc0a6e2b8728615d
AtbTheGreat <annatiffanybenjamin@gmail.com>
2015-03-24 19:34:39 -0400

    Created slow station
    Slow station makes the ship slower. Lulz

---
+++ Assets/Scripts/PropElements/SlowStation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowStation : MonoBehaviour {
+
+	private Rigidbody2D rigid;
+	private GameObject Player;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find ("Player");
+		rigid = Player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if(col.gameObject == Player){
+			rigid.drag = 3;
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		
+		if(col.gameObject == Player){
+			rigid.drag = 0;
+		}
+		
+	}
+
+}
